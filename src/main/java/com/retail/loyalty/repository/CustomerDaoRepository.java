faf70d54b536ea8416acef9fbc3120102600d5a7
kavithakmurthy <kavitha.kmurthy10@gmail.com>
2020-08-11 23:36:37 +0530

    Added mongo db repo layer

---
+++ src/main/java/com/retail/loyalty/repository/CustomerDaoRepository.java
@@ -0,0 +1,4 @@
+package com.retail.loyalty.repository;
+
+public interface CustomerDaoRepository {
+}
--- src/main/java/com/retail/loyalty/service/CustomerServiceImpl.java
+++ src/main/java/com/retail/loyalty/service/CustomerServiceImpl.java
@@ -2,31 +2,31 @@ package com.retail.loyalty.service;
 
 import com.retail.loyalty.models.Customer;
 import com.retail.loyalty.repository.CustomerRepository;
-import com.sun.el.stream.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CustomerServiceImpl implements CustomerService{
+public class CustomerServiceImpl implements CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
 
     @Autowired
-    private MongoTemplate mongoTemplate;
+    MongoOperations mongoOperations;
 
     public boolean createCustomer(Customer customer) {
         customerRepository.save(customer);
         return true;
     }
 
-    public boolean updateCustomer(long customerId,Customer customer) {
+    public boolean updateCustomer(long customerId, Customer customer) {
         Query query = new Query();
         query.addCriteria(Criteria.where("_id").is(customerId));
-        mongoTemplate.save(customer);
+        query.fields().include("_id");
+
+        Customer customerRepo = mongoOperations.findAndReplace(query, customer);
         return true;
 
     }
